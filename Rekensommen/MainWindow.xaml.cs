82260c9de7863433f81a66ecf5ccf9aa6c1b7931
Wim Bervoets <wim.bervoets@pxl.be>
2024-09-07 23:45:04 +0200

    init

--- Rekensommen/MainWindow.xaml.cs
+++ Rekensommen/MainWindow.xaml.cs
@@ -105,50 +105,10 @@ public partial class MainWindow : Window
         //TODO:
         //check if the input from resultTextBox is a number
         //check if the input is equal to _expectedResult
+        //change the backgroundcolor to lightgreen or lightcoral
         //TIP: use TryParse
 
         throw new NotImplementedException(); //Remove this line
-    }
-
-    private void Range_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        TextBox textBox = (TextBox)sender;
-        //TextBox textBox = sender as TextBox;
-
-        //By default the background is white
-        textBox.Background = Brushes.White;
-
-        if (int.TryParse(textBox.Text, out int number))
-        {
-            if (number < 0 || number > 100)
-            {
-                //If the number is out of range, the background will be lightcoral
-                textBox.Background = Brushes.LightCoral;
-            }
-        }
-        else
-        {
-            //If the input is not a number, the background will be lightcoral
-            textBox.Background = Brushes.LightCoral;
-        }
-
-        //If no condition is met, the background will still be white
-    }
-
-    private void Range_KeyDown(object sender, KeyEventArgs e)
-    {
-        if(e.Key >= Key.D0 && e.Key <= Key.D9 && e.KeyboardDevice.Modifiers == ModifierKeys.Shift)
-        {
-            e.Handled = false;
-        }
-        else if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
-        {
-            e.Handled = false;
-        }
-        else
-        {
-            e.Handled = true;
-        }
     }
 
     private void showTimeButton_Click(object sender, RoutedEventArgs e)
